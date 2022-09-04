77b7211b8aad4ebb7007b508aa11244b84755e4d
unknown <kristianachwan@gmail.com>
2022-09-05 01:51:26 +0800

    animation letter-falling added + timer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import HealthBar from './components/HealthBar';
 import { randomLetter } from './functions/randomLetter';
@@ -8,9 +8,21 @@ const barLengthArray = [100, 60, 20, 0]
 function App() {  
   const [barColor, setBarColor] = useState(barColorArray[0])
   const [barLength, setBarLength] = useState(barLengthArray[1]) 
+  const [counter, setCounter] = useState(30) 
+  // set initially as a winning condition
+  const [result, setResult] = useState('Congrats, you survive!')
+  useEffect(() => {
+    if (counter > 0){
+      setTimeout(() => setCounter(counter-1), 1000)
+    }
+
+  }, [counter])
   return (
     <div className="App">
-      <h1 className="title">TYPING FALL</h1>
+      <h1 className="title">TYPING FALL</h1> 
+      {/* <h3>Can you survive in 30s?</h3> */}
+      {counter > 0 ? <h1>{counter} s</h1> : <h1>{result}</h1>}
+      <h1 variant="" className="letter">A</h1>
       <HealthBar barColor={barColor} barLength={barLength}/>
     </div>
   );
