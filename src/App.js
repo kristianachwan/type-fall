ffc65b2292b3d9be7f1c79f849f2d3cd2fb152fa
unknown <kristianachwan@gmail.com>
2022-09-05 00:50:03 +0800

    title + healthbar design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './App.css';
-
-function App() {
+import HealthBar from './components/HealthBar';
+import { randomLetter } from './functions/randomLetter';
+import { Container } from '@mui/system';
+const barColorArray = [['#4caf50', '#66bb6a'], ['#ff9800', '#f57c00'], ['#bf360c', '#d84315'], [null, null]] 
+const barLengthArray = [100, 60, 20, 0] 
+function App() {  
+  const [barColor, setBarColor] = useState(barColorArray[0])
+  const [barLength, setBarLength] = useState(barLengthArray[1]) 
   return (
     <div className="App">
-     <h1>Hello world!</h1>
+      <h1 className="title">TYPING FALL</h1>
+      <HealthBar barColor={barColor} barLength={barLength}/>
     </div>
   );
 }
 
 export default App;
+
+
+
+// first we want to generate letter every 1s 
+// then we want to track the letter using array 
+// when we are able to 'kill' the letter OR the letter damages us, remove that element 
---
+++ src/components/HealthBar.js
@@ -0,0 +1,22 @@
+import { Box } from '@mui/material'
+import { Container } from '@mui/system'
+import React from 'react'
+import './HealthBar.css'
+export default function HealthBar({barColor, barLength}) {
+  return (    
+    <Box className="health-bar"  
+        maxWidth="90vw"
+        sx={{
+            width: `${barLength}%`,
+            height: 75,
+            borderRadius: 1, 
+            backgroundColor: barColor[0],
+            '&:hover': {
+            backgroundColor: barColor[1],
+            opacity: [0.9, 0.8, 0.7],
+            },
+        }}
+    />
+    
+  )
+}
