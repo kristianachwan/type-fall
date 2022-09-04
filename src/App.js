296e50ea1e764c489862bf77e9c490423cca29fa
unknown <kristianachwan@gmail.com>
2022-09-05 02:23:35 +0800

    added randomizer for pos + letters

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 import HealthBar from './components/HealthBar';
 import { randomLetter } from './functions/randomLetter';
 import { Container } from '@mui/system';
 const barColorArray = [['#4caf50', '#66bb6a'], ['#ff9800', '#f57c00'], ['#bf360c', '#d84315'], [null, null]] 
-const barLengthArray = [100, 60, 20, 0] 
+const barLengthArray = [100, 60, 20, 0]  
+
+// creating array of letters that are randomized in order (A-Z)
+
+const arr = [] 
+for(let i = 0; i<26;){
+  let letter = randomLetter()
+  let horizontalPos = Math.random() * 101
+  if (!arr.includes(letter)){
+    arr.push([letter, horizontalPos])
+    i++ 
+  }
+}
 function App() {  
+ 
+
   const [barColor, setBarColor] = useState(barColorArray[0])
   const [barLength, setBarLength] = useState(barLengthArray[1]) 
   const [counter, setCounter] = useState(30) 
   // set initially as a winning condition
   const [result, setResult] = useState('Congrats, you survive!')
   useEffect(() => {
+    // for timer 
     if (counter > 0){
       setTimeout(() => setCounter(counter-1), 1000)
     }
-
   }, [counter])
+
+
+
   return (
     <div className="App">
-      <h1 className="title">TYPING FALL</h1> 
-      {/* <h3>Can you survive in 30s?</h3> */}
-      {counter > 0 ? <h1>{counter} s</h1> : <h1>{result}</h1>}
-      <h1 variant="" className="letter">A</h1>
+      <div className="title-layer">
+        <h1 className="title">TYPING FALL</h1> 
+        {/* <h3>Can you survive in 30s?</h3> */}
+        {counter > 0 ? <h1>{counter} s</h1> : <h1>{result}</h1>}
+
+      </div>
+      {/* <h1 variant="" className="letter">A</h1> */} 
+
+      {arr && arr.map(letter => {
+        return (
+          <h1 variant="" style={{ left: `${letter[1]}vw`, transformX: "translate(-50%)"}} className="letter" key={letter[1]}>{letter[0]}</h1>
+        ) 
+      })}
       <HealthBar barColor={barColor} barLength={barLength}/>
     </div>
   );
--- src/functions/randomLetter.js
+++ src/functions/randomLetter.js
@@ -2,9 +2,5 @@ export function randomLetter () {
     // note that capital letters in ASCII range from 65-90 
 
     const charDec = parseInt(Math.random() * 27 + 65) 
-    return String.fromCharCode(charDec)
-     
-
-
-
+    return String.fromCharCode(charDec) 
 } 
